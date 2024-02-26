d58b1ed074771c7371315bc9fe6e30cf951a298d
Akhil Bolla <akhilbolla89@gmail.com>
2024-02-26 11:06:54 +0530

    la1

--- src/components/Blogs/BlogCard.jsx
+++ src/components/Blogs/BlogCard.jsx
@@ -18,7 +18,16 @@ export default function BlogCard({ className, datas }) {
         />
       </div>
       <div className="p-[24px]">
-        <div className="short-data flex space-x-9 items-center mb-3">
+        <div className="details">
+          <a to="/blogs/blog">
+            <h1 className="text-[20px] text-qblack hover:text-blue-500 font-semibold line-clamp-2 mb-1 capitalize lg:text-[18px]">
+              {datas.title}
+            </h1>
+          </a>
+          <p className="text-qgraytwo text-[16px] leading-[30px] line-clamp-2 mb-3 lg:text-[14px]">
+            {datas.article}
+          </p>
+          <div className="short-data flex space-x-9 items-center mb-3">
           <div className="flex space-x-1.5 items-center">
           <IcoPeople />
             <span className="text-sm text-qgraytwo capitalize">
@@ -32,15 +41,6 @@ export default function BlogCard({ className, datas }) {
             </span>
           </div>
         </div>
-        <div className="details">
-          <a to="/blogs/blog">
-            <h1 className="text-[20px] text-qblack hover:text-blue-500 font-semibold line-clamp-2 mb-1 capitalize lg:text-[18px]">
-              {datas.title}
-            </h1>
-          </a>
-          <p className="text-qgraytwo text-[16px] leading-[30px] line-clamp-2 mb-3 lg:text-[14px]">
-            {datas.article}
-          </p>
           <a href="#">
             <div className="flex items-center space-x-2">
               <span className="text-qblack text-sm font-semibold">
