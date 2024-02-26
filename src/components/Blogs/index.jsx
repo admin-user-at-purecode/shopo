1edbae7fd6fae81a61d55bfd6f7239627bf787d9
Akhil Bolla <akhilbolla89@gmail.com>
2024-02-26 13:27:47 +0530

    displaying 3 blog cards

--- src/components/Blogs/index.jsx
+++ src/components/Blogs/index.jsx
@@ -21,7 +21,7 @@ export default function Blogs() {
       <div className="w-full py-[60px]">
         <div className="container-x mx-auto">
           <div className="w-full">
-            <div className="grid md:grid-cols-2 grid-cols-1 lg:gap-[30px] gap-5">
+            <div className="grid md:grid-cols-2 grid-cols-1 lg:gap-[16px] gap-5 lg:grid-cols-3">
               <DataIteration
                 datas={blog.blogs}
                 startLength={0}
